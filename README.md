525c7b8e1237c5b3a0b13bc21ce43b1425269ad7
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-06-06 17:38:20 +0200

    Last change of the day

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# php-oop-booking-app
+# PHP OOP Booking App
+
+## Description
+
 The aim op this project is to make an OOP based Booking App in PHP
+
+## Features
+
+1. The ability to sign in and out
+2. The ability to search for specific accommodations
+3. The ability to compare two accommodations
+4. The ability to book reservations
+5. The ability to view previous bookings
+
+## MVP
+
+- Forms must have frontend validation
+- User must be allowed to compare two accommodations
+- Each hotel must have a different daily rate
+- When the user clicks book an email must be populated and sent to the users email address
+- Data must persist between pages
+
+## Backlog
+
+- Add Sign In/Register pages and json files
+- Add booking page and comparison
+- Add ability to search for accommodations
+- Add in email extension via Composer
+
+## Links
+
+### Repo link
+
+https://github.com/OliverVermeulen/php-oop-booking-app.git
--- index.php
+++ index.php
@@ -16,9 +16,9 @@
 
 <body>
     <header>
-        <img src="./src/images/airdnd-2.png" alt="" class="logo">
+        <img src="./src/images/airdnd.png" alt="Airdnd logo" class="logo">
 
-        <form action="">
+        <form action="#" method="POST">
             <input type="search" name="search" class="search-input" placeholder="Search">
             <button type="submit" class="search-btn"><i class="fa fa-search"></i></button>
         </form>
@@ -31,55 +31,55 @@
     <main>
         <div class="grid-container">
             <div class="grid-item">
-                <img src="./src/images/bag-end.jpg" alt="" class="accommodation">
+                <img src="./src/images/bag-end.jpg" alt="Bag End" class="accommodation">
                 <p>The Shire, Ariandor</p>
                 <p>R1000 ZAR night</p>
             </div>
             <div class="grid-item">
-                <img src="./src/images/adams-family-mansion.jpg" alt="" class="accommodation">
-                <p>Los Angeles, USA</p>
+                <img src="./src/images/addams-family-mansion.jpg" alt="Addams Family Mansion" class="accommodation">
+                <p>Finding, Lost Angeles</p>
                 <p>R2000 ZAR night</p>
             </div>
             <div class="grid-item">
-                <img src="./src/images/simpsons.jpg" alt="" class="accommodation">
+                <img src="./src/images/simpsons-house.jpg" alt="The Simpsons House" class="accommodation">
                 <p>Springfield, Oregon</p>
                 <p>R900 ZAR night</p>
             </div>
             <div class="grid-item">
-                <img src="./src/images/spongebob-house.jpg" alt="" class="accommodation">
+                <img src="./src/images/pineapple-house.jpg" alt="Pineapple House" class="accommodation">
                 <p>Bathurst, Eastern Cape</p>
                 <p>R1000 ZAR night</p>
             </div>
 
             <div class="grid-item">
-                <img src="./src/images/minecraft.jpg" alt="" class="accommodation">
+                <img src="./src/images/minecraft-house.jpg" alt="Minecraft Village House" class="accommodation">
                 <p>Village, Grass Plains</p>
                 <p>R1000 ZAR night</p>
             </div>
             <div class="grid-item">
-                <img src="./src/images/tardis.jpg" alt="" class="accommodation">
+                <img src="./src/images/tardis.jpg" alt="Tardis" class="accommodation">
                 <p>Anywhere, Galaxy</p>
                 <p>R10000 ZAR night</p>
             </div>
             <div class="grid-item">
-                <img src="./src/images/flintstones.jpg" alt="" class="accommodation">
+                <img src="./src/images/flintstones-house.jpg" alt="The Flintstones House" class="accommodation">
                 <p>Bedrock, Cobblestone County</p>
                 <p>R1200 ZAR night</p>
             </div>
             <div class="grid-item">
-                <img src="./src/images/teletubby-dome.jpg" alt="" class="accommodation">
-                <p>Anywhere, Galaxy</p>
+                <img src="./src/images/teletubby-homedome.jpg" alt="Teletubby Homedome" class="accommodation">
+                <p>Homedome, Teletubbyland</p>
                 <p>R10000 ZAR night</p>
             </div>
 
             <div class="grid-item">
-                <img src="./src/images/the-burrow.jpg" alt="" class="accommodation">
+                <img src="./src/images/the-burrow.jpg" alt="The Burrow" class="accommodation">
                 <p>Devon, England</p>
                 <p>R1500 ZAR night</p>
             </div>
 
             <div class="grid-item">
-                <img src="./src/images/kame-house.jpg" alt="" class="accommodation">
+                <img src="./src/images/kame-house.jpg" alt="Kame house" class="accommodation">
                 <p>Island, Polynesia</p>
                 <p>R1500 ZAR night</p>
             </div>
