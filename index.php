c36a88fe2288ba3533f52b6d94cc54973e05fbfd
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-06-06 13:20:54 +0200

    big push

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-echo "Hello World";
 ?>
 
 <!DOCTYPE html>
@@ -8,22 +7,31 @@ echo "Hello World";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign In</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Looking.com</title>
+    <link rel="stylesheet" href="./src/css/style.css">
 </head>
 <body>
-    <form action="#" method="post" class="sign-in-form">
-        <label for="name">Enter name</label>
-        <input type="text" name="name" placeholder="Enter name only" required>
+    <header>
+        <h1>Looking.com</h1>
+        <div class="register_sign-in">
+            <a href="#">Register</a>
+            <a href="#">Sign In</a>
+        </div>
+    </header>
+    <main>
+        <!-- <form action="#" method="post" class="sign-in-form">
+            <label for="name">Enter name</label>
+            <input type="text" name="name" placeholder="Enter name only" required>
 
-        <label for="surname">Enter surname</label>
-        <input type="text" name="surname" placeholder="Enter surname only" required>
+            <label for="surname">Enter surname</label>
+            <input type="text" name="surname" placeholder="Enter surname only" required>
 
-        <label for="email">Enter email</label>
-        <input type="text" name="surname" placeholder="Enter email" required>
+            <label for="email">Enter email</label>
+            <input type="text" name="surname" placeholder="Enter email" required>
 
-        <label for="password">Enter password</label>
-        <input type="password" name="password" maxlength="20" required>
-    </form>
+            <label for="password">Enter password</label>
+            <input type="password" name="password" maxlength="20" required>
+        </form> -->
+    </main>
 </body>
 </html>
