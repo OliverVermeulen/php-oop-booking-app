b22e1ed946815da35870db999558b34b106561c4
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-06-07 18:14:42 +0200

    large update

--- index.php
+++ index.php
@@ -29,56 +29,56 @@
         </div>
     </header>
     <main>
-        <div class="grid-container">
-            <div class="grid-item">
+        <div class="cards">
+            <div class="card">
                 <img src="./src/images/bag-end.jpg" alt="Bag End" class="accommodation">
                 <p>The Shire, Ariandor</p>
                 <p>R1000 ZAR night</p>
             </div>
-            <div class="grid-item">
-                <img src="./src/images/addams-family-mansion_640x480.jpg" alt="Bag End" class="accommodation">
+            <div class="card">
+                <img src="./src/images/addams-family-mansion.jpg" alt="The Addams Family Mansion" class="accommodation">
                 <p>Finding, Lost Angeles</p>
                 <p>R2000 ZAR night</p>
             </div>
-            <div class="grid-item">
-                <img src="./src/images/ezgif-2-f01cd044da_640x480.jpg" alt="Bag End" class="accommodation">
+            <div class="card">
+                <img src="./src/images/kame-house.jpg" alt="Kame House" class="accommodation">
                 <p>Island, Polynesia</p>
                 <p>R1500 ZAR night</p>
             </div>
-            <div class="grid-item">
-                <img src="./src/images/flintstones_640x480.jpg" alt="Bag End" class="accommodation">
+            <div class="card">
+                <img src="./src/images/the-flintstones-house.jpg" alt="The Flintstones House" class="accommodation">
                 <p>Bedrock, Cobblestone County</p>
                 <p>R1200 ZAR night</p>
             </div>
 
-            <div class="grid-item">
-                <img src="./src/images/minecraft_640x480.jpg" alt="Bag End" class="accommodation">
+            <div class="card">
+                <img src="./src/images/minecraft-village-house.jpg" alt="Minecraft Village House" class="accommodation">
                 <p>Village, Grass Plains</p>
                 <p>R1000 ZAR night</p>
             </div>
-            <div class="grid-item">
-                <img src="./src/images/tardis_640x480.jpg" alt="Bag End" class="accommodation">
+            <div class="card">
+                <img src="./src/images/tardis.jpg" alt="The Tardis" class="accommodation">
                 <p>Anywhere, Galaxy</p>
                 <p>R10000 ZAR night</p>
             </div>
-            <div class="grid-item">
-                <img src="./src/images/teletubby-dome_640x480.jpg" alt="Bag End" class="accommodation">
+            <div class="card">
+                <img src="./src/images/tubbytronic-superdome.jpg" alt="Tubbytronic Superdome" class="accommodation">
                 <p>Homedome, Teletubbyland</p>
                 <p>R10000 ZAR night</p>
             </div>
-            <div class="grid-item">
-                <img src="./src/images/spongebob-house_640x480.jpg" alt="Bag End" class="accommodation">
+            <div class="card">
+                <img src="./src/images/spongebob-pineapple-house.jpg" alt="Pineapple House" class="accommodation">
                 <p>Bathurst, Eastern Cape</p>
                 <p>R1000 ZAR night</p>
             </div>
 
-            <div class="grid-item">
-                <img src="./src/images/the-burrow_640x480.jpg" alt="Bag End" class="accommodation">
+            <div class="card">
+                <img src="./src/images/the-burrow.jpg" alt="The Burrow" class="accommodation">
                 <p>Devon, England</p>
                 <p>R1500 ZAR night</p>
             </div>
-            <div class="grid-item">
-                <img src="./src/images/simpsons_640x480.jpg" alt="Bag End" class="accommodation">
+            <div class="card">
+                <img src="./src/images/the-simpsons-house.jpg" alt="The Simpsons House" class="accommodation">
                 <p>Springfield, Oregon</p>
                 <p>R900 ZAR night</p>
             </div>
