622a0055c0e8c13251ccd42dd59bbf1a6a3ff193
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-06-08 16:49:24 +0200

    update

--- src/include/header.inc.php
+++ src/include/header.inc.php
@@ -1,17 +1,14 @@
 <header class="header">
     <img src="/php-oop-booking-app/src/images/airdnd.png" alt="Airdnd logo" class="logo">
 
-    <form action="#" method="POST">
-        <input type="search" name="search" class="search-input" placeholder="Search">
+    <form action="#" method="POST" class="search-bar">
+        <input type="search" name="search" class="form-input" placeholder="Search">
         <button type="submit" class="search-btn"><i class="fa fa-search"></i></button>
     </form>
 
     <div class="register_sign-in">
         <a href="/php-oop-booking-app/src/pages/register.php" class="navigation-button">Register</a>
-        <a href="/php-oop-booking-app/src/pages/register.php" class="navigation-button">Sign In</a>
-
-        <!-- Trigger/Open The Modal -->
-        <button id="myBtn">Sign In</button>
+        <button id="myBtn" class="sign-in-button">Sign In</button>
         <!-- The Modal -->
         <div id="myModal" class="modal">
             <!-- Modal content -->
@@ -21,9 +18,11 @@
                     <h2>Sign In</h2>
                 </div>
                 <div class="modal-body">
-                    <p>Some text in the Modal Body</p>
-                    <p>Some other text...</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat aliquam adipisci perferendis dolores quidem ad quibusdam odit commodi animi, eius a sequi enim vitae, quae nostrum aliquid. Sapiente, accusamus perspiciatis.</p>
+                    <form action="" method="post">
+                        <input type="text" name="email" class="form-input" placeholder="Email">
+                        <input type="password" name="password" class="form-input" placeholder="Password">
+                        <input type="submit" class="sign-in-button" value="Sign In">
+                    </form>
                 </div>
             </div>
 
