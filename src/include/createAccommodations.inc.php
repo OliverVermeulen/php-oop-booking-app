6316c926a665da7376b6f98bd79807556541d531
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-06-13 15:24:08 +0200

    fixed booking

--- src/include/createAccommodations.inc.php
+++ src/include/createAccommodations.inc.php
@@ -7,7 +7,7 @@ function createAccommodations($accommodations)
             <div class='card'>
                 <img src=" . $accommodationArray['image'] . " alt=" . $accommodationArray['name'] . " class='accommodation'>
                 <form action='/php-oop-booking-app/src/pages/booking.php'>
-                    <button class='info-modal'><i class='fa fa-info'></i></button>
+                    <button class='booking-page-btn'><i class='fa fa-info'></i></button>
                 </form>
                 <p>" . $accommodationArray['location'] . "</p>
                 <p>R" . $accommodationArray['price'] . " ZAR night</p>
