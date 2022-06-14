b520937d4dede142ac9a7bdf8f8523e8aae2e070
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-06-14 14:19:52 +0200

    large update

--- src/include/header.inc.php
+++ src/include/header.inc.php
@@ -1,32 +1,8 @@
 <header class="header">
     <img src="/php-oop-booking-app/src/images/airdnd.png" alt="Airdnd logo" class="logo">
 
-    <form action="#" method="POST" class="search-bar">
-        <input type="search" name="search" class="form-input" placeholder="Search">
-        <!-- <button type="submit" class="search-btn"><i class="fa fa-search"></i></button> -->
-    </form>
-
-    <div class="register_sign-in_modal">
-        <div class="register_sign-in">
-            <form action="/php-oop-booking-app/src/pages/register.php">
-                <input type="submit" value="Register" class="sign-in-button">
-            </form>
-            <button id="myBtn" class="sign-in-button">Sign In</button>
-        </div>
-        <div id="myModal" class="modal">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <span class="close">&times;</span>
-                    <h2>Sign In</h2>
-                </div>
-                <div class="modal-body">
-                    <form action="" method="post" class="register_sign-in_form">
-                        <input type="text" name="email" class="form-input" placeholder="Email">
-                        <input type="password" name="password" class="form-input" placeholder="Password">
-                        <input type="submit" class="sign-in-button" value="Sign In">
-                    </form>
-                </div>
-            </div>
-        </div>
+    <div class="social-media">
+        <a href="https://www.linkedin.com/in/oliver-vermeulen-311221222/" target="_blank" class="media-icon"><i class="fa fa-linkedin-square" alt="Linkedin"></i></a>
+        <a href="https://github.com/OliverVermeulen" target="_blank" class="media-icon"><i class="fa fa-github-square" alt="Github"></i></a>
     </div>
 </header>
