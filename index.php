7cc4dd1c24aae3f002cab7847d4ff5ec766e8f5c
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-06-15 14:48:54 +0200

    save

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ $head = "Airdnd";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="The aim of this project is to make a OOP based booking app in PHP">
     <title><?= $head ?></title>
     <link rel="stylesheet" href="/php-oop-booking-app/src/css/style.css">
     <link rel="shortcut icon" href="/php-oop-booking-app/src/images/tab-icon.png" type="image/x-icon">
