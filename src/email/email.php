190c860b3c1f93a36bb5bf74865bfabba702ecfb
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-06-21 13:47:30 +0200

    large update

---
+++ src/email/email.php
@@ -0,0 +1,36 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require '/MAMP/htdocs/php-oop-booking-app/vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Recipients
+    $mail->setFrom('oliver13xbox360@gmail.com', 'Mailer');
+    $mail->addAddress('oliververmeulen862@gmail.com', 'Oliver Vermeulen');     //Add a recipient
+    $mail->addAddress('oliververmeulen862@gmail.com');               //Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>Work fucker</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
