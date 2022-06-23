a6b81fda2a5a152b05a0458a0b6810d4129b1bda
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-06-23 12:19:39 +0200

    added in PHPMailer an Mailtrap.io functionality

--- src/email/email.php
+++ src/email/email.php
@@ -1,42 +1,38 @@
 <?php
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-// Load Composer's autoloader
 require '/MAMP/htdocs/php-oop-booking-app/vendor/autoload.php';
 
-// Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.mailtrap.io';                     // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'b2d7d1788a8c08';                       // SMTP username
-    $mail->Password   = 'de45537a61dbc0';                       // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 2525;                                   // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.mailtrap.io';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = '64baf55dfaac57'; // For those marking just put in your own Mailtrap Username and Password
+    $mail->Password   = 'f72bf18f5f77da';
+    $mail->Port       = 2525;
 
     //Recipients
-    $mail->setFrom('oliver13xbox360@gmail.com', 'Mailer');
-    $mail->addAddress('oliververmeulen862@gmail.com', 'Oliver Vermeulen');     // Add a recipient
-    $mail->addAddress('oliververmeulen862@gmail.com');               // Name is optional
-    $mail->addReplyTo('oliver13xbox360@gmail.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
-
+    $mail->setFrom('confirmation@airdnd.com', 'Airdnd'); // Sender
+    $mail->addAddress('oliververmeulen862@gmail.com', 'Juan Vermeulen'); // Receiver
+    $mail->addReplyTo('reply@airdnd.com', 'Reply'); // Reply
 
     // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
+    $mail->isHTML(true);
+    $mail->Subject = 'Airdnd Booking confirmation';
+    $mail->Body    = '
+                    <p>Hi Juan,</p>
+                    <p>You have recently made a reservation for Bag End located in The Shire, Ariandor starting from 12/04/2022 till 14/04/2022</p>
+                    <p>You total costs are: R2000.00 ZAR</p>
+                    <br>
+                    <p>Regards, Airdnd</p>
+                    ';
     $mail->send();
     echo 'Message has been sent';
 } catch (Exception $e) {
